29b8cd29bff829b18eeb1a6630bfb1c2ed4ad602
Nuocheng Shen <shennu@oregonstate.edu>
2021-04-30 20:55:40 -0700

    fix test
    decide to make a new value of test

--- Q2/listAvgTest.py
+++ Q2/listAvgTest.py
@@ -3,8 +3,8 @@ import listAvg
 
 class testCase(unittest.TestCase):
     def test_listAvg_base(self):
-        test_list_1 = [1,2,3,4,5,6,7,8,9,0]
-        self.assertEqual(listAvg.avg(test_list_1), 4.5 )
+        test_list_1 = [1,2,3,4,5,6,7,8,9]
+        self.assertEqual(listAvg.avg(test_list_1), 5 )
     
     def test_listAvg_digit(self):
         test_list_2 = [1,2,7]
