13b4d3e8e9efd05918d6e5be0a3d140030863948
Nuocheng Shen <shennu@oregonstate.edu>
2021-04-30 21:34:06 -0700

    add test case to Q3
    Add test case:
    2. Abbreviations
    3. Not a name

--- Q3/fullName.py
+++ Q3/fullName.py
@@ -1,5 +1,11 @@
 def nameGenerate(fName, lName):
 
-    fullName = fName + " " + lName
+    if (isinstance(fName, str) == 0  or isinstance(lName, str) == 0):
+        raise ValueError('Name should be a string.')
+    else:
+        if (len(fName) == 1):
+            fullName = lName + ", " + fName + "."
+        else:
+            fullName = fName + " " + lName
 
     return fullName
--- Q3/fullNameTest.py
+++ Q3/fullNameTest.py
@@ -3,9 +3,20 @@ import fullName
 
 class testCase(unittest.TestCase):
     def test_fullName_base(self):
-        fName = "fName"
-        lName = "lName"
-        self.assertEqual(fullName.nameGenerate(fName, lName), "fName lName")
+        fName_1 = "fName"
+        lName_1 = "lName"
+        self.assertEqual(fullName.nameGenerate(fName_1, lName_1), "fName lName")
+
+    def test_fullName_Abb(self):
+        fName_2 = "F"
+        lName_2 = "lName"
+        self.assertEqual(fullName.nameGenerate(fName_2, lName_2), "lName, F.")
+
+    def test_fullName_str(self):
+        fName_3 = "1234"
+        lName_3 = "lName"
+        with self.assertRaises(ValueError):
+            fullName.nameGenerate(fName_3, lName_3)
 
 
 if __name__ == '__main__':
